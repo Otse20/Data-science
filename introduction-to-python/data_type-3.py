a381b75aba19174dc0c3cd2e3c27e6b829cbc703
Otse20 <ayekhemheesther@gmail.com>
2024-10-15 15:20:22 +0100

    Update data_type-3.py

--- introduction-to-python/data_type-3.py
+++ introduction-to-python/data_type-3.py
@@ -1,3 +1,5 @@
+#Write a Python program to take a list of numbers, numbers = [10, 20, 30, 40, 50], and convert it into a tuple. Print both the original list and the converted tuple.
+
 numbers = [10,20,30,40,50]
 numbers_tuple = tuple(numbers)
 print(numbers)
