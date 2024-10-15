0933328b4b7e87b78d181415bddf01ca5cbbd667
Otse20 <ayekhemheesther@gmail.com>
2024-10-15 15:25:10 +0100

    Update function-2.py

--- introduction-to-python/function-2.py
+++ introduction-to-python/function-2.py
@@ -1,3 +1,5 @@
+#Write a Python function square (n) that takes a number n as input and returns the square of that number. Test the function by calling it with the number 5 and printing the result.
+
 def square (n):
     return n**2
 sum =square (5)
