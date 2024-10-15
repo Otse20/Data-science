bf0b21b7a1a2a7b39335868f08fce225b38a1245
Otse20 <ayekhemheesther@gmail.com>
2024-10-15 15:17:15 +0100

    Update variable-2.py

--- introduction-to-python/variable-2.py
+++ introduction-to-python/variable-2.py
@@ -1,3 +1,5 @@
+#Write a Python program to assign a string value to a variable called message and print it. Then, change the value of message and print the new value.
+
 message = "my name is Esther"
 print(message)
 message = "favourite"
