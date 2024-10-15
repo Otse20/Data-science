ab6eeed94f01def0b903ef9d9aa76366788260c2
Muideen27 <ilorimuideen0000@gmail.com>
2024-10-15 18:24:22 +0100

    string

---
+++ Tutor-folder/0-string.py
@@ -0,0 +1,7 @@
+# Basic understanding of sting
+
+first_name = "Esther"
+last_name = 'Ayekhemhe'
+age = 24
+print(f"my name is {first_name} and my surname is {last_name}") # format string
+print("My first name is", first_name, "and my surame is", last_name) # Normal way
