b9322ebdb13173ca2c1e7bddb8f5299d1a6d6bf8
Otse20 <ayekhemheesther@gmail.com>
2024-10-15 15:17:52 +0100

    Update variable-3.py

--- introduction-to-python/variable-3.py
+++ introduction-to-python/variable-3.py
@@ -1,3 +1,5 @@
+#Write a Python program to swap the values of two variables, x and y, without using a third variable. Print the values of x and y before and after the swap.
+
 x = 5
 y = 10
 print(x,y)
