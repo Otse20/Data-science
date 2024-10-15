4ff87f53769ab7984f32b51fcfd0f6783d16a023
Otse20 <ayekhemheesther@gmail.com>
2024-10-15 15:19:29 +0100

    Update data_type-2.py

--- introduction-to-python/data_type-2.py
+++ introduction-to-python/data_type-2.py
@@ -1,3 +1,5 @@
+#Given a string s = "12345", write a Python program to convert this string into an integer and print the result. Then, convert it back to a string and print the result again.
+
 s = "12345"
 num = int(s)
 print(num)
