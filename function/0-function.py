428bff6f3cf6290f0e325e3b04e1647805888503
Muideen27 <ilorimuideen0000@gmail.com>
2024-10-18 21:06:38 +0100

    teaching python function

---
+++ function/0-function.py
@@ -0,0 +1,15 @@
+# introduction to functions
+
+def add_num(num1, num2): 
+    # num1 and num2 are parameters
+    # def is the definition of a function
+    # add_num is the name of the function
+    return num1 + num2
+
+# end of function
+sum = add_num(2, 3) # 2 and 3 are arguments
+print(f"The sum is {sum}")
+
+
+
+    
